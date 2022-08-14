4f94ef253af36c8b7c361876fa6fe25ac17927d9
Angel Galarza <angel_alonso1@outlook.es>
2022-08-13 23:57:01 -0500

    Add Validate function

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import PySimpleGUI as sg
 
+def Validate(input):
+    bits = []
+    for i in input:
+        if i == '0' or i == '1':   
+            bits.append(i)
+        else:
+            return None
+    return bits
+
 def main():
-    pass
+    layout = [ 
+        [sg.Text('Bits'), sg.Input(key = '-Bits-'), sg.Button('Graficar', key = '-Graficar-')]
+    ]
 
-
+    window = sg.Window('Codificacion Binaria', layout)
+    while True:
+        event, values = window.Read()
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+        if event == '-Graficar-':
+            input = values['-Bits-']
+            bits = Validate(input)
+            if bits is not None:
+                print('valores correctos')
+            else:
+                print('valores incorrectos')
+            
+    window.close()
 if __name__ == '__main__':
     main()
